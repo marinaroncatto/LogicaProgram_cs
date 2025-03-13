35637c13dc8d335f0f5d22d3bc053ff5ce8e66e9
marinaroncatto <marinam.roncatto@gmail.com>
2025-03-13 12:38:45 -0300

    Aula 1
    Comandos iniciais de entrada e saída

---
+++ c_sharp/PrimeiroPrograma/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PrimeiroPrograma;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //escreve uma mensagem na tela e quebra linha
+        // para não quebrar a linha usar "Console.Write("")"
+    }
+}
---
+++ c_sharp/Saudacao/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Saudacao;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.Write("Informe seu nome: ");
+
+        string nome = Console.ReadLine(); //lê toda a linha
+
+        Console.WriteLine("Ola, "+nome+ "!");
+
+
+    }
+}
