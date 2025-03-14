74d3def61c79f071e2287b572d9202f1b08f37e0
marinaroncatto <marinam.roncatto@gmail.com>
2025-03-14 11:27:30 -0300

    Estruturas condicionais

---
+++ c_sharp/AreaCirculo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AreaCirculo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Raio: ");
+        double raio = double.Parse(Console.ReadLine());
+
+        double area = 3.14159 * (raio * raio);
+
+        Console.WriteLine("Area: {0:N2}", area); // formata a saída com 2 casas decimais
+
+        
+    }
+}
---
+++ c_sharp/Autenticacao/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Autenticacao;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        String nome, senha;
+
+        Console.WriteLine("Digite o nome do usuário: ");
+        nome = Console.ReadLine();
+
+        Console.WriteLine("Digite a senha: ");
+        senha = Console.ReadLine();
+
+        if(nome == "Maria" && senha == "123ca"){
+            Console.WriteLine("Acesso Liberado");
+            for(int i = 0; i<=10; i++){
+                if(i % 2 == 0){
+                    Console.WriteLine("Valor: "+i);
+                }
+            }
+        }else{
+            Console.WriteLine("Acesso negado");
+        }    
+    }
+}
