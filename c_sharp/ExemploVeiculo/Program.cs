7a5be266fc77bc26af87712729c744a6278d9f7c
marinaroncatto <marinam.roncatto@gmail.com>
2025-03-17 17:27:03 -0300

    web 4 - desvio condicional

---
+++ c_sharp/ExemploVeiculo/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExemploVeiculo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Velocidade: ");
+        int velocidade = int.Parse(Console.ReadLine());
+
+        if(velocidade > 70){
+            Console.WriteLine("Multado");
+        }else{
+            Console.WriteLine("Não Multado");
+        }
+
+        Console.WriteLine("Fim");
+        
+    }
+}
