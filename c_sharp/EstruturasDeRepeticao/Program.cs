d1ed669a6f75131f816cdd139d03cbfce9186fda
marinaroncatto <marinam.roncatto@gmail.com>
2025-03-18 11:16:45 -0300

    Estruturas de repetição
    While e For

---
+++ c_sharp/EstruturasDeRepeticao/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EstruturasDeRepeticao;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int controle = 0;
+
+        while(controle < 10){
+
+            Console.WriteLine("Bom dia!");
+            controle = controle + 1;
+        }
+        
+        for(int i = 0; i < 10; i++){
+            Console.WriteLine("Bom tarde!");
+        }
+    }
+}
---
+++ c_sharp/ExemploWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExemploWhile;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int soma = 0;        
+        int num;
+
+        Console.WriteLine("Digite um valor: ");
+        num = int.Parse(Console.ReadLine());
+
+        while(num >= 0){           
+            soma = soma + num;
+            Console.WriteLine("Digite um valor: ");
+            num = int.Parse(Console.ReadLine());                            
+        }
+        Console.WriteLine("O total é: "+ soma);
+        Console.WriteLine("fim");
+    }
+}
