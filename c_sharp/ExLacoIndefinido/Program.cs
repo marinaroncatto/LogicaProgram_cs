5fab7e5e527675d1ecd754fa15157517d12c7964
marinaroncatto <marinam.roncatto@gmail.com>
2025-03-19 11:36:41 -0300

    Estrutura de repetição
    Laço definido e indefinido

---
+++ c_sharp/ExLacoIndefinido/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExLacoIndefinido;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int soma = 0;
+
+        Console.WriteLine("Número: ");
+        int n = int.Parse(Console.ReadLine());
+
+        while(n >=0){
+            soma += n; // soma = soma + n
+            Console.WriteLine("Número: ");
+            n = int.Parse(Console.ReadLine());
+        } 
+
+        Console.WriteLine("Soma = {0}", soma);
+
+        
+    }
+}
---
+++ c_sharp/ExemploFor/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExemploFor;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        for(int i = 1; i <= 10; i++){
+            Console.WriteLine("Algoritimos");
+        }
+
+
+        
+    }
+}
