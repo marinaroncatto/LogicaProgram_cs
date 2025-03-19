4801f788912f9ba87b70daabda5ef40b4d775028
marinaroncatto <marinam.roncatto@gmail.com>
2025-03-19 15:35:45 -0300

    funções

---
+++ c_sharp/ExFuncao1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ExFuncao1;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe seu nome: ");
+        string umNome = Console.ReadLine();
+        //chamar a função
+        digaOla(umNome);
+    }
+
+    //procedimento - função que nao devolve resposta (void)
+
+    // declaração da função
+    static void digaOla(string nome){
+        Console.WriteLine("Olá, {0}!", nome);
+    }
+}
---
+++ c_sharp/ExFuncao2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ExFuncao2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Primeiro Valor: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Segundo Valor: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int m = maior(a, b);
+        Console.WriteLine("Maior: {0}", m);
+    }
+
+    static int maior(int n1, int n2){
+        int maior;
+        if(n1 >= n2){
+            maior = n1;
+        }else{
+            maior = n2;
+        }
+        return maior;
+    }
+}
