362e13a6e9513715c409377f17a1ceccb9de31db
marinaroncatto <marinam.roncatto@gmail.com>
2025-03-23 21:44:05 -0300

    praticando

---
+++ c_sharp/Teste/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Teste;
+
+class Program{
+
+        //programa para dizer o maior entre 10 números
+
+public static void Main(string[] args){
+
+        int maiorNumero = 0;
+
+        for(int i = 0; i < 10; i++){
+                Console.WriteLine("Digite um número: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                if(maiorNumero < numero){
+                        maiorNumero = numero;
+                }
+
+        }
+
+        Console.WriteLine("Maior número: {0}", maiorNumero);
+
+}
+
+}
