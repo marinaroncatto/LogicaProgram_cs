a6aebdae631b9b4e5cca0273298f9c89b28486e0
marinaroncatto <marinam.roncatto@gmail.com>
2025-05-22 11:30:35 -0300

    Aula sobre Vetores

---
+++ c_sharp_2/aula1/MediaVetores/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MediaVetores;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-- PROGRAMA DA MÉDIA --");
+
+        Console.WriteLine("Informe a quantidade de notas: ");
+        int tam = Convert.ToInt32(Console.ReadLine());
+
+        double[] notas = new double[tam];
+
+        for (int cont = 0; cont < notas.Length; cont++)
+        {
+            Console.WriteLine("Informe a nota:");
+            double nota = Double.Parse(Console.ReadLine());
+
+            notas[cont] = nota;
+        }
+
+        double soma = 0;
+        foreach (double item in notas)
+        {
+            soma = soma + item;
+        }
+
+        double media = soma / notas.Length;
+
+        string situacao = "";
+        if (media >= 6)
+        {
+            situacao = "Aprovado";
+        }
+        else
+        {
+            situacao = "Reprovado";
+        }
+
+        Console.WriteLine("Média: " + media);
+        Console.WriteLine("Situação: " + situacao);
+    }
+}
---
+++ c_sharp_2/aula1/Vetores/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Vetores;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //criar vetores com valores iniciais
+        int[] A = { 15, 10, 3 };
+
+
+        //criar vetores com uma qtd de compartimentos
+        int qtd = 3;
+        int[] B = new int[qtd];
+
+        //altera valores das posições
+        int pos = 0;
+        B[pos] = 10;
+        B[1] = 7;
+        B[2] = 8;
+
+        //lê os valores das posições
+        int pos1 = 0;
+        int valor1 = B[pos1];
+        int valor2 = B[1];
+
+        Console.WriteLine(valor1);
+
+        //tamanho do vetor
+        int tam = B.Length;
+
+        Console.WriteLine(tam);
+
+        //iterar sobre o vetor usando o FOR
+        for (int cont = 0; cont < B.Length; cont++)
+        {
+            int item = B[cont];
+            Console.WriteLine(item);
+        }
+
+        //iterar sobre o vetor usando FOREACH
+        foreach (int item in B)
+        {
+            Console.WriteLine(item);
+        }
+
+    }
+}
