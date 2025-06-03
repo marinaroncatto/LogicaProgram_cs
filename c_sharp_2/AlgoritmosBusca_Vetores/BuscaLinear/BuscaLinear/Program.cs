9f561628a6a26b21f88df4d022edfceaba20e281
marinaroncatto <marinam.roncatto@gmail.com>
2025-06-03 11:04:02 -0300

    Busca Linear

---
+++ c_sharp_2/AlgoritmosBusca_Vetores/BuscaLinear/BuscaLinear/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BuscaLinear;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Busca Linear em Vetores");
+
+        int[] dados = { 52, 17, 69, 84, 3, 26, 83, 54, 19, 50 };
+        int valor_procurado = 54;
+        bool valor_encontrado = false;
+
+        for (int i = 0; i < dados.Length; i++)
+        {
+            if (dados[i] == valor_procurado)
+            {
+                Console.WriteLine("Valor encontrado no índice " + i);
+                valor_encontrado = true;
+                break;
+            }
+        }
+        if (!valor_encontrado)
+        {
+            Console.WriteLine("Valor não encontrado");
+        }
+
+
+
+    }
+}
