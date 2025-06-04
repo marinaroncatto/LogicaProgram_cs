87964f411d1d5115f4479af8dab9d0485309b27b
marinaroncatto <marinam.roncatto@gmail.com>
2025-06-04 19:54:36 -0300

    Insertion Sort

---
+++ c_sharp_2/AlgOrdenacao_elementares/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace InsertionSort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Insertion Sort");
+        int[] vetor = { 99, 82, 50, 67, 90, 20, 71, 8, 21, 18 };
+        int num, pos_verificada;
+        ImprimirVetor(vetor);
+
+        for (int pos = 1; pos < vetor.Length; pos++)
+        {
+            num = vetor[pos];
+            pos_verificada = pos - 1;
+            while (pos_verificada >= 0 && num < vetor[pos_verificada])
+            {
+                vetor[pos_verificada + 1] = vetor[pos_verificada];
+                pos_verificada--;
+            }
+            vetor[pos_verificada + 1] = num;
+        }
+        ImprimirVetor(vetor);
+    }
+
+    public static void ImprimirVetor(int[] vetor)
+    {
+        Console.WriteLine("\nImprimindo Vetor\n");
+        foreach (var numero in vetor)
+        {
+            Console.WriteLine(numero);            
+        }
+    }
+
+}
