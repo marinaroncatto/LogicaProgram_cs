539b97ef6e580b98428bbe64ff2d0d35d11f00c8
marinaroncatto <marinam.roncatto@gmail.com>
2025-06-04 19:42:15 -0300

    Selection Sort

---
+++ c_sharp_2/AlgOrdenacao_elementares/SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SelectionSort;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Selection Sort");
+
+        int[] vetor = { 99, 82, 50, 67, 90, 20, 71, 8, 21, 18 };
+        int min, temp;
+        ImprimirVetor(vetor);
+        for (int i = 0; i < vetor.Length; i++)
+        {
+            min = i;
+            for (int pos = (i + 1); pos < vetor.Length; pos++)
+            {
+                if (vetor[pos] < vetor[min])
+                {
+                    min = pos;
+                }
+            }
+            if (vetor[i] != vetor[min])
+            {
+                temp = vetor[i];
+                vetor[i] = vetor[min];
+                vetor[min] = temp;
+            }
+        }
+        ImprimirVetor(vetor);
+    }
+
+    public static void ImprimirVetor(int[] vetor)
+    {
+        Console.WriteLine("\nImprimindo Vetor\n");
+        foreach (var numero in vetor)
+        {
+            Console.WriteLine(numero);            
+        }
+    }
+
+}
