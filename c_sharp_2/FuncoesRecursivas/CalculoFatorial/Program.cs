f6467c8f775511a1f84af6c95ec93928c88552d8
marinaroncatto <marinam.roncatto@gmail.com>
2025-06-05 11:43:13 -0300

    Recursividade 1: ExFatorial

---
+++ c_sharp_2/FuncoesRecursivas/CalculoFatorial/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace CalculoFatorial;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calculo Fatorial com Recursividade");
+        int respostaIterativa = fatIterativo(3);
+        Console.WriteLine("Fatorial Iterativo: "+respostaIterativa);
+        int respostaRecursiva = fatRecursivo(3);
+        Console.WriteLine("Fatorial Recursivo: "+respostaRecursiva);
+    }
+
+
+    public static int fatRecursivo(int numero)
+    {
+        if (numero == 1 || numero == 0)
+            return 1;
+        else
+            return numero * fatRecursivo(numero - 1);
+    }
+
+    public static int fatIterativo(int numero)
+    {
+        int resultado = 1;
+        while (numero >= 1)
+        {
+            resultado = resultado * numero;
+            numero = numero - 1;
+        }
+        return resultado;
+    }
+}
